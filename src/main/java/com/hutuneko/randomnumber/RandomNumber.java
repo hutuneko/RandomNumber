aa408716586bc2f883cd9537b181ab8dac290992
hutuneko <mtknykc1104@icloud.com>
2026-01-11 20:33:30 +0900

    おそらくこれ以降変えることはないでしょう

--- src/main/java/com/hutuneko/randomnumber/RandomNumber.java
+++ src/main/java/com/hutuneko/randomnumber/RandomNumber.java
@@ -1,9 +1,5 @@
 package com.hutuneko.randomnumber;
 
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(RandomNumber.MODID)
--- src/main/java/com/hutuneko/randomnumber/mixin/MixinFont.java
+++ src/main/java/com/hutuneko/randomnumber/mixin/MixinFont.java
@@ -4,7 +4,6 @@ import com.hutuneko.randomnumber.NumberMaps;
 import net.minecraft.client.gui.Font;
 import net.minecraft.util.FormattedCharSequence;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -24,14 +23,10 @@ public class MixinFont {
 
         StringBuilder sb = new StringBuilder();
         for (char c : text.toCharArray()) {
-            // 前回作成した RandomNumber.NUMBER_MAP を使用
             sb.append(NumberMaps.getNumberMap().getOrDefault(c, c));
         }
         return sb.toString();
     }
-    /**
-     * FormattedCharSequence（チャット、看板、％表示など）の描画最深部をフックします。
-     */
     @ModifyVariable(
             method = "drawInternal(Lnet/minecraft/util/FormattedCharSequence;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/client/gui/Font$DisplayMode;II)I",
             at = @At("HEAD"),
@@ -39,10 +34,7 @@ public class MixinFont {
     )
     private FormattedCharSequence randomNunber$scrambleInternalSequence(FormattedCharSequence original) {
         if (original == null) return null;
-
-        // 元のシーケンスをラップし、文字(codePoint)が処理される瞬間に数字を差し替える
         return (visitor) -> original.accept((index, style, codePoint) -> {
-            // RandomNumber.NUMBER_MAP2 (Integer用マップ) を使用してコードポイントを変換
             int newCodePoint = NumberMaps.getNumberMap2().getOrDefault(codePoint, codePoint);
             return visitor.accept(index, style, newCodePoint);
         });
